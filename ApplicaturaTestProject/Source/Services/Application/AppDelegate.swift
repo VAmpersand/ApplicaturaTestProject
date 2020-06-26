d9e81af962fe5c1ec0809c27f0cce7903d6f9ee0
VAmpersand <VP_ampersand_P@icloud.com>
2020-06-26 15:24:22 +0300

    Added bases CoreData

--- ApplicaturaTestProject/Source/Services/Application/AppDelegate.swift
+++ ApplicaturaTestProject/Source/Services/Application/AppDelegate.swift
@@ -1,12 +1,37 @@
 import UIKit
+import CoreData
 
 @UIApplicationMain
-class AppDelegate: UIResponder {}
+class AppDelegate: UIResponder {
+    lazy var persistentContainer: NSPersistentContainer = {
+        let container = NSPersistentContainer(name: "CityData")
+        container.loadPersistentStores(completionHandler: { (storeDescription, error) in
+            if let error = error as NSError? {
+                fatalError("Unresolved error \(error), \(error.userInfo)")
+            }
+        })
+        return container
+    }()
+}
 
 extension AppDelegate: UIApplicationDelegate {
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+    func application(_ application: UIApplication,
+                     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         
         return true
     }
 }
 
+extension AppDelegate {
+    func saveContext () {
+        let context = persistentContainer.viewContext
+        if context.hasChanges {
+            do {
+                try context.save()
+            } catch {
+                let nserror = error as NSError
+                fatalError("Unresolved error \(nserror), \(nserror.userInfo)")
+            }
+        }
+    }
+}
